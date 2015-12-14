9a234542943befd1207608c2f508cf2d05e984f8
Eric Berryman <berryman@frib.msu.edu>
2015-12-14 15:04:58 -0500

    test

--- build/travis.sh
+++ build/travis.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 folderChangeSet={`git diff-tree --name-status HEAD`}
-for((i=0; i<${#folderChangeSet[@]}; i++))
+for i in "${folderChangeSet[@]}"
 do
-  if [ ${folderChangeSet[$i]} == "M" ]
-  then
+  if [ ${folderChangeSet[$i]} == "M" ]; then
     project=${folderChangeSet[$i+1]}
     print $project
   fi
